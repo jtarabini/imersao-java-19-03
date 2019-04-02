4714fd35567a8c77bee09cc8a8679077b35806b8
Thiago Valverde de Souza <valverde.thiago@gmail.com>
2019-04-02 11:11:46 -0300

    Adicionando sql com a criação das sequences

---
+++ sql/create_sequences.sql
@@ -0,0 +1,10 @@
+CREATE SEQUENCE public.equipamento_id_seq;
+
+ALTER SEQUENCE public.equipamento_id_seq
+    OWNER TO postgres;
+	
+	
+CREATE SEQUENCE public.carro_id_seq;
+
+ALTER SEQUENCE public.carro_id_seq
+    OWNER TO postgres;
