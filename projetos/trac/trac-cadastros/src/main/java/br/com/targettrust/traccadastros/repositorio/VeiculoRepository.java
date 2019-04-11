e095b38256573c1cba3c47829af55ebbdf89d9e5
Thiago Valverde de Souza <valverde.thiago@gmail.com>
2019-04-11 12:07:43 -0300

    Adicionando relacionamentos do projeto

---
+++ projetos/trac/trac-cadastros/src/main/java/br/com/targettrust/traccadastros/repositorio/VeiculoRepository.java
@@ -0,0 +1,9 @@
+package br.com.targettrust.traccadastros.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.targettrust.traccadastros.entidades.Veiculo;
+
+public interface VeiculoRepository extends JpaRepository<Veiculo, Long>{
+
+}
