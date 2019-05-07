e4f09257fcf55ad8e72e4d6338533b0a55897afd
Thiago Valverde de Souza <valverde.thiago@gmail.com>
2019-05-06 22:00:36 -0300

    Implementando deleção de todos os modelos de uma dada marca

--- projetos/trac/trac-cadastros/src/main/java/br/com/targettrust/traccadastros/repositorio/ModeloRepository.java
+++ projetos/trac/trac-cadastros/src/main/java/br/com/targettrust/traccadastros/repositorio/ModeloRepository.java
@@ -5,10 +5,21 @@ import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.targettrust.traccadastros.entidades.Modelo;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ModeloRepository extends JpaRepository<Modelo, Long>{
 
 	@Transactional
 	void deleteByNome(String nome);
 
+	@Query("delete Modelo modelo "+
+			" where modelo.id in( "+
+			"        select id "+
+			"          from Modelo "+
+			"         where marca.nome = :marca )")
+	@Transactional
+	@Modifying
+    void deleteByMarca(@Param("marca") String marca);
 }
