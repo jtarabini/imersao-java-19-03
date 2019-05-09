ab6e8189d908f45cc8129d2978ea9d40e6208df7
jtarabini <aluno04sala04@targettrust.local>
2019-05-09 12:18:55 -0300

    inicio tema

--- projetos/trac/trac-cadastros/src/main/java/br/com/targettrust/traccadastros/controller/FuncionarioController.java
+++ projetos/trac/trac-cadastros/src/main/java/br/com/targettrust/traccadastros/controller/FuncionarioController.java
@@ -3,12 +3,19 @@ package br.com.targettrust.traccadastros.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,6 +47,30 @@ public class FuncionarioController {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
 	}
+	@DeleteMapping("/{id}")
+	public void deleteById(@PathVariable("id") Long id) {
+		usuarioRepository.deleteById(id);
+	}
+	
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public HttpEntity<Funcionario> createFuncionario(@Valid @RequestBody Funcionario funcionario) {
+		if(funcionario == null || funcionario.getId() != null) {
+			return ResponseEntity.badRequest().build();
+		}
+		return ResponseEntity.ok(usuarioRepository.save(funcionario));		
+	}
+	
+	@PutMapping(value="/{id}", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public HttpEntity<Funcionario> updateFuncionario(@PathVariable("id") Long id, 
+			@Valid @RequestBody Marca marca) {
+		Optional<Usuario> dbFuncionario = usuarioRepository.findById(id);
+		if(dbFuncionario.isPresent()) {
+			dbFuncionario.get().setNome(marca.getNome());
+			dbFuncionario.get().setVersion(marca.getVersion());	
+			usuarioRepository.save(dbFuncionario.get());
+			return ResponseEntity.ok().build();
+		}
+		return ResponseEntity.notFound().build();		
+	}
 
 }
-
