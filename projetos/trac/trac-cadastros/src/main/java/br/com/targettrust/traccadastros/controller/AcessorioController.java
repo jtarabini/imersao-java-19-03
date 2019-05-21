12287f841c09f74cd85b578531ebeae20982decd
viniantunes <vinicius.mantunes@gmail.com>
2019-05-21 09:09:39 -0300

    mm

--- projetos/trac/trac-cadastros/src/main/java/br/com/targettrust/traccadastros/controller/AcessorioController.java
+++ projetos/trac/trac-cadastros/src/main/java/br/com/targettrust/traccadastros/controller/AcessorioController.java
@@ -4,23 +4,18 @@ package br.com.targettrust.traccadastros.controller;
 import java.util.List;
 import java.util.Optional;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.targettrust.traccadastros.entidades.Acessorio;
+import br.com.targettrust.traccadastros.entidades.Modelo;
 import br.com.targettrust.traccadastros.repositorio.AcessorioRepository;
 
 @RestController
@@ -55,30 +50,15 @@ public class AcessorioController {
 		else {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
+		
 	}
-
-	@DeleteMapping("{id}")
-	public void deleteById(@PathVariable("id") Long id) {
-		acessorioRepository.deleteById(id);
-	}
-	
-	@PostMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public HttpEntity<Acessorio> createAcessorio(@Valid @RequestBody Acessorio acessorio) {
-		if(acessorio == null || acessorio.getId() != null) {
-			return ResponseEntity.badRequest().build();
-		}
-		return ResponseEntity.ok(acessorioRepository.save(acessorio));
-	}
-	
-	@PutMapping(value="/{id}", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public HttpEntity<Acessorio> updateMarca(@PathVariable("id") Long id, @Valid @RequestBody Acessorio acessorio) {
-		Optional<Acessorio> dbAcessorio = acessorioRepository.findById(id);
-		if(dbAcessorio.isPresent()) {
-			dbAcessorio.get().setDescricao(acessorio.getDescricao());
-			acessorioRepository.save(dbAcessorio.get());
-			return ResponseEntity.ok().build();
-		}
-		return ResponseEntity.notFound().build();		
+	@GetMapping("/search")
+	public HttpEntity<List<Acessorio>> search(
+			@RequestParam(name="id", required=false) Long id, 
+			@RequestParam(name="descricao", required=false) String descricao)	 {
+		System.out.println(id);
+		System.out.println(descricao);
+		return ResponseEntity.ok(acessorioRepository.search(id, descricao));
 	}
 }
 
--- projetos/trac/trac-cadastros/src/main/java/br/com/targettrust/traccadastros/repositorio/AcessorioRepository.java
+++ projetos/trac/trac-cadastros/src/main/java/br/com/targettrust/traccadastros/repositorio/AcessorioRepository.java
@@ -3,9 +3,13 @@ package br.com.targettrust.traccadastros.repositorio;
 import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.targettrust.traccadastros.entidades.Acessorio;
+import br.com.targettrust.traccadastros.entidades.Marca;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AcessorioRepository extends JpaRepository<Acessorio, Long>{
@@ -16,4 +20,11 @@ public interface AcessorioRepository extends JpaRepository<Acessorio, Long>{
 	@Transactional
 	Optional<Acessorio> findByDescricao(String descricao);
 
+	@Query("   select marca "
+			+ "  from Marca marca "
+			+ " where (:id is null or marca.id = :id )"
+			+ "   and (:nome is null or marca.nome = :nome )")
+	List<Marca> search(
+			@Param("id") Long id, 
+			@Param("nome") String nome);
 }
