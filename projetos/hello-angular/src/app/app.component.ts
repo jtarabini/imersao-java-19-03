c3136366ecfb04b91e16a06b948bfbe0331a4a85
Thiago Valverde de Souza <valverde.thiago@gmail.com>
2019-05-28 12:45:05 -0300

    Incluindo projeto angular

---
+++ projetos/hello-angular/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'TRAC';
+  subtitle = 'TargetTrust Rent A Car';
+  version = 1;
+}
---
+++ projetos/hello-angular/src/app/user-cmp/user-cmp.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-cmp',
+  templateUrl: './user-cmp.component.html',
+  styleUrls: ['./user-cmp.component.css']
+})
+export class UserCmpComponent 
+  implements OnInit {
+  userList : User[] = [
+    { 
+      firstName : "Thiago ", 
+      middleName : "Valverde de",
+      lastName : "Souza"
+    },
+    { 
+      firstName : "Simoni ", 
+      middleName : "Scudero",
+      lastName : "Kowalsky"
+    },
+    { 
+      firstName : "Carmem", 
+      middleName : "Suzana",
+      lastName : "Vaz"
+    }
+
+  ];
+  selectedUser : User = this.userList[0];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  selectUser(user) {
+    this.selectedUser = user;
+  }
+
+}
+
+export class User {
+  firstName : String;
+  middleName : String;
+  lastName : String;
+}
